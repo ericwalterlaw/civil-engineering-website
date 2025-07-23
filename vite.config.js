80ad787944b7474e1e22f0f1ece64e1362458562
cosuoji <qelvinz@gmail.com>
2025-07-23 21:27:24 +0100

    Updated

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,16 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 
-// https://vite.dev/config/
+// vite.config.js
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  base: '/', 
+  plugins: [react(), tailwindcss()],
   build: {
-    outDir: 'dist', // default
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html' // Ensure this points to your entry HTML file
+      }
+    }
   }
 })
